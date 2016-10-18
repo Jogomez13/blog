a7c4d7703fd09c43332055df4c5bff6abfccdcb4
jogomez <jonathan.gomez.web@gmail.com>
2016-10-18 21:47:41 +0200

    Supp AppBundle,correction block body,ajout template sur inscri et connex

--- src/BlogBundle/Controller/BlogController.php
+++ src/BlogBundle/Controller/BlogController.php
@@ -2,14 +2,13 @@
 
 namespace BlogBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Routing\Annotation\Route;
 
 
 class BlogController extends Controller {
     /**
-     * @Route("/accueil", name = "accueil")
-     * 
+     * @Route("/accueil", name="accueil")
      */
     public function indexAction()
     {
