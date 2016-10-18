36f313a3a9b9489893de5c716d38cbd01ff22697
jogomez <jonathan.gomez.web@gmail.com>
2016-10-18 15:40:38 +0200

    Création AdminViewController,structure html:inscription,connexion,admin

--- src/AdminBundle/Controller/AdminController.php
+++ src/AdminBundle/Controller/AdminController.php
@@ -2,16 +2,10 @@
 
 namespace AdminBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdminController extends Controller
 {
-    /**
-     * @Route("/")
-     */
-    public function indexAction()
-    {
-        return $this->render('AdminBundle:Default:index.html.twig');
-    }
+   
+    
 }
