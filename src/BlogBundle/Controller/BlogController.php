5aed8fbc7be9387c5879b92c494223e0d28a09f4
jogomez <jonathan.gomez.web@gmail.com>
2016-10-18 17:33:52 +0200

    Création des entity

--- src/BlogBundle/Controller/BlogController.php
+++ src/BlogBundle/Controller/BlogController.php
@@ -2,16 +2,45 @@
 
 namespace BlogBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
-class BlogController extends Controller
-{
+
+class BlogController extends Controller {
     /**
-     * @Route("/")
+     * @Route("/accueil", name = "accueil")
+     * 
      */
     public function indexAction()
     {
-        return $this->render('BlogBundle:Default:index.html.twig');
+        return $this->render('BlogBundle:Default:accueil.html.twig');
+    }
+    /**
+     * @Route("/telephone", name="telephone")
+     */
+    public function indexTelephone()
+    {
+        return $this->render('BlogBundle:Default:telephone.html.twig');
+    }
+    /**
+     * @Route("/tablette", name="tablette")
+     */
+    public function indexTablette()
+    {
+        return $this->render('BlogBundle:Default:tablette.html.twig');
+    }
+    /**
+     * @Route("/ordinateur", name="ordinateur")
+     */
+    public function indexOrdi()
+    {
+        return $this->render('BlogBundle:Default:ordi.html.twig');
+    }
+    /**
+     * @Route("/membre", name="membre")
+     */
+    public function indexEspaceMembre()
+    {
+        return $this->render('BlogBundle:Default:contact.html.twig');
     }
 }
