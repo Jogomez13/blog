25d57b0857fb6f65cf7e5e703a225bc92fb59900
jogomez <jonathan.gomez.web@gmail.com>
2016-10-19 14:53:27 +0200

    Création des routes pour les vues côtés admin et début structure html

--- src/AdminBundle/Controller/AdminViewController.php
+++ src/AdminBundle/Controller/AdminViewController.php
@@ -4,7 +4,6 @@
 namespace AdminBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
@@ -14,8 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  */
 class AdminViewController extends Controller {
      /**
-     * @Route("/admin")
-     * @Template("AmdinBundle:Default:admin.html.twig")
+     * @Route("/admin", name="admin")
      */
     public function getAdmin()
     {
@@ -23,20 +21,51 @@ class AdminViewController extends Controller {
     }
     
      /**
-     * @Route("/admin/connexion", name="connexion")
-     * @Template("AmdinBundle:Default:connexion.html.twig")
+     * @Route("/connexion", name="connexion")
      */
     public function GetConnexion()
     {
-        return $this->render('AdminBundle:Default:connexion.html.twig');
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+
+           return $this->redirectToRoute("bool"); //Retour sur la page principal//             
+       }
+       
+       $authenticationUtils = $this->get('security.authentication_utils');
+       return $this->render('AdminBundle:Default:connexion.html.twig', array(
+                   'last_username' => $authenticationUtils->getLastUsername(),
+                   'error' => $authenticationUtils->getLastAuthenticationError(),
+       ));
     }
     
     /**
-     * @Route("/admin/inscription", name="inscription")
-     * @Template("AmdinBundle:Default:inscription.html.twig")
+     * @Route("/inscription", name="inscription")
      */
     public function GetInscription()
     {
         return $this->render('AdminBundle:Default:inscription.html.twig');
+    }
+    
+     /**
+     * @Route("/profil", name="profil")
+     */
+    public function GetProfil()
+    {
+        return $this->render('AdminBundle:Default:profil.html.twig');
+    }
+    
+     /**
+     * @Route("/modifprofil", name="modifprofil")
+     */
+    public function GetModifprofil()
+    {
+        return $this->render('AdminBundle:Default:modifprofil.html.twig');
+    }
+    
+    /**
+     * @Route("/brouillions", name="brouillons")
+     */
+    public function GetBrouillons()
+    {
+        return $this->render('AdminBundle:Default:brouillons.html.twig');
     }
 }
